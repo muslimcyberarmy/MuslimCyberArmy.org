db17ca64151faf67e2914e319a2005e04af70d0f
MCA - Anon <dakwahmedia.official@gmail.com>
2018-02-25 13:51:56 +0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      :mini-variant="true"
+      fixed
+      app
+      dark
+      disable-route-watcher
+      v-model="drawer"
+      class="grey darken-4"
+    >
+      <v-list>
+        <v-list-tile
+          router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed app class="blue accent-4 white--text elevation-0">
+      <v-toolbar-side-icon @click="drawer = !drawer" class="white--text"></v-toolbar-side-icon>
+      <input type="text" placeholder="Search" class="mca-input">
+      <v-spacer></v-spacer>
+      <v-btn
+        icon
+      >
+        <v-icon class="white--text">apps</v-icon>
+      </v-btn>
+      <!--<v-toolbar-title v-text="title"></v-toolbar-title>-->
+    </v-toolbar>
+    <v-content>
+      <v-container fluid class="pa-0">
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span class="f-normal">&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'trending_up', title: 'Trends', to: '/s' },
+          { icon: 'lightbulb_outline', title: 'Dakwah', to: '/ss' }
+        ],
+        title: 'MuslimCyberArmy.org'
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-layout row>
+    <v-flex xs9 class="white text-xs-center">
+      <v-layout>
+        <v-flex xs4 pa-3>
+          FACT
+        </v-flex>
+        <v-flex xs4 pa-3>
+          COMPARABLE
+        </v-flex>
+        <v-flex xs4 pa-3>
+          HOAX
+        </v-flex>
+      </v-layout>
+    </v-flex>
+    <v-flex xs3 class="pa-3 grey lighten-4 text-xs-center">
+      TRENDS
+    </v-flex>
+  </v-layout>
+</template>
